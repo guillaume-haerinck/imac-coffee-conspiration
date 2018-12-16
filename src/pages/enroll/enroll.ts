3dac7ce4b08be4717f40f629f9fc21bf3ce2aa1d
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2018-12-16 18:31:00 +0100

    avatar service + debut carte

--- src/pages/enroll/enroll.ts
+++ src/pages/enroll/enroll.ts
@@ -1,14 +1,21 @@
 import "./enroll.scss";
 import "../../shared/custom-elements/pc-window/pc-window";
 import { QuotesService } from "./services/quotes.service";
+import { AvatarsService } from "./services/avatars.service";
 
-/*
+
 const quotesService = new QuotesService();
 quotesService.getQuote()
   .then(quote => {
-    console.log(quote);
+    document.getElementById("citation").innerHTML = `"${quote.quote}"`;
   })
   .catch(error => {
     console.warn("cannot get quote");
   })
-*/
+
+const avatarsService = new AvatarsService();
+avatarsService.getAvatar()
+  .then(avatar => {
+    const image = document.getElementById("agent-picture-img") as HTMLImageElement;
+    image.src = avatar.picture.thumbnail;
+  })
