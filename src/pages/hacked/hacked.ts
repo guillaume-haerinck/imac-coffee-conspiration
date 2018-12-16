13b6575544e833322e9f77630b70aaf537d9d3f8
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2018-12-16 20:06:36 +0100

    hacked mac crash

--- src/pages/hacked/hacked.ts
+++ src/pages/hacked/hacked.ts
@@ -1,1 +1,14 @@
 import "./hacked.scss";
+
+
+document.addEventListener("DOMContentLoaded", function(event){
+    document.getElementById(`mac-crash`).style.display = 'block';
+});
+
+document.onmousedown = function() {
+    console.log("man");
+    
+    setTimeout(function(){ 
+        document.getElementById(`mac-crash`).style.display = 'none';
+    }, 3000);
+}
