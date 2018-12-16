68ff3214f022e8f7f8c65666eaeced45b7b32768
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2018-12-16 21:47:02 +0100

    quizz features

--- src/pages/quizz/quizz.ts
+++ src/pages/quizz/quizz.ts
@@ -5,6 +5,28 @@ import { RoverXpDog } from "./elements/rover-xp-dog/rover-xp-dog";
 /* Exports to access elements inside html */
 (window as any).RoverXpDog = new RoverXpDog("background-wrapper");
 
+document.getElementById('wallpaper').addEventListener('dblclick',(e)=>{
+  document.getElementById('background-wrapper').style.backgroundImage = `url(${e.target.currentSrc})`;
+  document.getElementById('background-wrapper').style.backgroundSize = `cover`;
+  document.getElementById('virtual-girl').style.display = `block`;
+});
+
+document.getElementById('flight-simulator').addEventListener('dblclick',(e)=>{
+  document.getElementById('crack').style.display = `grid`;
+  document.getElementById('youtube-crack').src = `https://www.youtube.com/embed/sODZLSHJm6Q?controls=0&autoplay=1&version=3&enablejsapi=1`;
+});
+
+document.querySelectorAll('#crack .control')[0].addEventListener('click',(e)=>{
+  console.log("here");
+  
+  document.getElementById('crack').style.display = `none`;
+  document.getElementById('youtube-crack').src = ``;
+
+});
+
+console.log("lol");
+
+
 function checkTime(i) {
     if (i < 10) {
       i = "0" + i;
