fa36c51432a70b0dd595cb283a37b8b50f88bba2
Nicolas LIENART <nicolas@lienart.net>
2018-12-18 20:25:08 +0100

    Netscape update

--- src/shared/custom-elements/pc-window/pc-window.ts
+++ src/shared/custom-elements/pc-window/pc-window.ts
@@ -109,8 +109,6 @@ export class PCWindow extends HTMLElement {
         this.style.position = "absolute";
         const posX = Math.floor(Math.random() * Math.floor(window.innerWidth - 400));
         const posY = Math.floor(Math.random() * Math.floor(window.innerHeight - 500));
-        console.log(window.innerWidth - this.offsetWidth)
-        console.log('positions : ',posX, posY);
         this.style.left = posX.toString() + "px";
         this.style.top = posY.toString() + "px";
     }
